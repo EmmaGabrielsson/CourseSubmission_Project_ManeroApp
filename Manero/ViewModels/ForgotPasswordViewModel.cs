c1e08efe77ba3f7b52a695e410622e9e41e9e91d
Bianca Molnar <biancascorreos@hotmail.de>
2023-11-14 15:54:53 +0100

    validation for login and reset password forms

--- Manero/ViewModels/ForgotPasswordViewModel.cs
+++ Manero/ViewModels/ForgotPasswordViewModel.cs
@@ -1,11 +1,13 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace Manero.ViewModels;
-
-public class ForgotPasswordViewModel
+namespace Manero.ViewModels
 {
-    [Display(Name = "E-mail Address")]
-    [Required(ErrorMessage = "E-mail is required")]
-    [DataType(DataType.EmailAddress)]
-    public string Email { get; set; } = null!;
+    public class ForgotPasswordViewModel
+    {
+        [Display(Name = "E-mail Address")]
+        [Required(ErrorMessage = "E-mail is required")]
+        [DataType(DataType.EmailAddress)]
+        [RegularExpression(@"^[^\s@]+@[^\s@]+\.[^\s@]+$", ErrorMessage = "Invalid email format")]
+        public string Email { get; set; } = null!;
+    }
 }
