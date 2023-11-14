477c7df25a809b71c023b4c149bb083b96c63f4a
Bianca Molnar <biancascorreos@hotmail.de>
2023-11-14 16:03:44 +0100

    update regular expression

--- Manero/ViewModels/ForgotPasswordViewModel.cs
+++ Manero/ViewModels/ForgotPasswordViewModel.cs
@@ -7,7 +7,7 @@ namespace Manero.ViewModels
         [Display(Name = "E-mail Address")]
         [Required(ErrorMessage = "E-mail is required")]
         [DataType(DataType.EmailAddress)]
-        [RegularExpression(@"^[^\s@]+@[^\s@]+\.[^\s@]+$", ErrorMessage = "Invalid email format")]
+        [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "Invalid email format")]
         public string Email { get; set; } = null!;
     }
 }
