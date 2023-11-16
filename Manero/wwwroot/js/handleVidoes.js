66c7033fc934c24befac9036ea52a4a71a4d2be2
Emma Gabrielsson <111791152+dreamworks0601@users.noreply.github.com>
2023-11-16 23:40:26 +0100

    fixed null checks and some video settings for mobile

--- Manero/wwwroot/js/handleVidoes.js
+++ Manero/wwwroot/js/handleVidoes.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    // Get all video elements
     const videos = {
         kids: document.getElementById('myVideo-kids'),
         woman: document.getElementById('myVideo-woman'),
@@ -9,34 +8,45 @@ document.addEventListener("DOMContentLoaded", function () {
         bag: document.getElementById('myVideo-bag'),
         hat: document.getElementById('myVideo-hat'),
         shoes: document.getElementById('myVideo-shoes')
-
     };
 
-    // Add event listeners for hover for each video container
     Object.keys(videos).forEach(videoKey => {
         const videoContainer = document.querySelector(`.video-container-${videoKey}`);
         if (videoContainer) {
-            videoContainer.addEventListener('mouseover', () => playVideo(videoKey));
-            videoContainer.addEventListener('mouseout', () => pauseVideo(videoKey));
-        } else {
-            console.error(`Element with class 'video-container-${videoKey}' not found.`);
+            const video = videos[videoKey];
+            if (video) {
+                // Use 'mouseover' for desktop
+                videoContainer.addEventListener('mouseover', () => playVideo(videoKey));
+
+                // Use 'click' for mobile
+                if ('ontouchstart' in document.documentElement) {
+                    videoContainer.addEventListener('click', (event) => {
+                        event.preventDefault(); // Prevent navigation on click for mobiles
+                        playVideo(videoKey);
+                    });
+                }
+
+                videoContainer.addEventListener('mouseout', () => pauseVideo(videoKey));
+            } else {
+                console.error(`Video element with id 'myVideo-${videoKey}' not found.`);
+            }
         }
     });
 
-    // Function to play the video
     function playVideo(videoKey) {
         const video = videos[videoKey];
-
-        video.play().catch(error => {
-            // Handle play() promise rejection due to autoplay policy
-            console.error("Autoplay not allowed:", error);
-        });
+        if (video) {
+            video.play().catch(error => {
+                console.error("Autoplay not allowed:", error);
+            });
+        }
     }
 
-    // Function to pause the video
     function pauseVideo(videoKey) {
         const video = videos[videoKey];
-        video.pause();
+        if (video) {
+            video.pause();
+        }
     }
 
 });
