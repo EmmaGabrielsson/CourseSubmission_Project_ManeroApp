3db34822d036717a3eb4912b320c002c68b82998
Bianca Molnar <biancascorreos@hotmail.de>
2023-11-16 18:28:16 +0100

    succeeded login-test added

---
+++ Manero.Tests/DatabaseTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using Manero.Controllers;
+using Manero.Models.Entities;
+using Manero.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Moq;
+
+namespace Manero.Tests;
+
+public class DatabaseTests
+{
+    [Fact] 
+    public void ConnectionString_SqlServer_ShouldBeAConnectionStringToLocalSqlDatabase()
+    {
+        //Arange
+        var expected = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Besitzer\\Documents\\NewManeroDB.mdf;Integrated Security=True;Connect Timeout=30";
+
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        //Act
+        var result = configuration.GetConnectionString("SqlDatabase");
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+}
---
+++ Manero.Tests/LoginTests.cs
@@ -0,0 +1,57 @@
+﻿using Manero.Controllers;
+using Manero.Models.Entities;
+using Manero.ViewModels;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+
+namespace Manero.Tests
+{
+    public class LoginTests
+    {
+        [Fact]
+        public async Task Index_Post_ValidModel_ReturnsRedirectToActionResult()
+        {
+            // Arrange
+            var userStoreMock = new Mock<IUserStore<UserEntity>>();
+            var userManagerMock = new Mock<UserManager<UserEntity>>(userStoreMock.Object, null!, null!, null!, null!, null!, null!, null!, null!);
+
+            var httpContextAccessorMock = new Mock<IHttpContextAccessor>();
+            var userClaimsPrincipalFactoryMock = new Mock<IUserClaimsPrincipalFactory<UserEntity>>();
+            var signInManagerMock = new Mock<SignInManager<UserEntity>>(
+                userManagerMock.Object,
+                httpContextAccessorMock.Object,
+                userClaimsPrincipalFactoryMock.Object,
+                It.IsAny<IOptions<IdentityOptions>>(),
+                It.IsAny<ILogger<SignInManager<UserEntity>>>(),
+                It.IsAny<IAuthenticationSchemeProvider>(),
+                It.IsAny<IUserConfirmation<UserEntity>>()
+            );
+
+            var controller = new LoginController(signInManagerMock.Object, userManagerMock.Object);
+
+            var viewModel = new LoginViewModel
+            {
+                Email = "test@example.com",
+                Password = "Test@123",
+                RememberMe = false
+            };
+
+            userManagerMock.Setup(x => x.FindByEmailAsync(viewModel.Email)).ReturnsAsync(new UserEntity());
+            signInManagerMock.Setup(x => x.PasswordSignInAsync(It.IsAny<UserEntity>(), viewModel.Password, viewModel.RememberMe, false))
+                            .ReturnsAsync(Microsoft.AspNetCore.Identity.SignInResult.Success);
+
+            // Act
+            var result = await controller.Index(viewModel) as RedirectToActionResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal("Index", result.ActionName);
+            Assert.Equal("Home", result.ControllerName);
+        }
+    }
+}
