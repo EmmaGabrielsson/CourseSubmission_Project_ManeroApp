539d4dce074e256845c470edf9d3402a7c5c84c9
tommyBjorkman <tommy.bjorkman@utb.ecutbildning.se>
2023-11-17 12:08:02 +0100

    failed approach

--- Manero.Tests/CarouselTest.cs
+++ Manero.Tests/CarouselTest.cs
@@ -1,12 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
-namespace Manero.Tests
+namespace Manero.Tests;
+
+
+
+public class CarouselTest
 {
-    internal class CarouselTest
+    private IWebDriver? _driver;
+
+    [Fact]
+    public void TestInitialize()
     {
+        _driver = new ChromeDriver();
+    }
+    [Fact]
+    public void Carousel_Script_Should_Load()
+    {
+        _driver.Navigate().GoToUrl("https://localhost:7073/");
+
+        // Replace "your-app-url" with the actual URL of your application.
+
+        // Perform assertions
+        IWebElement jsFile = _driver.FindElement(By.CssSelector("script[src*='carousel.js']"));
+
+        Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsNotNull(jsFile, "JS file is not loaded on the main page.");
+    }
+
+    [Fact]
+    public void TestCleanup()
+    {
+        _driver.Quit();
     }
 }
