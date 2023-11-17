8503d210e2a2f7bfb7a2c44d8c645e467ced469f
Christopher <opheralm@gmail.com>
2023-11-17 11:04:47 +0100

    Tests

---
+++ Manero.Tests/OrderHistoryServiceTests.cs
@@ -0,0 +1,90 @@
+﻿using Manero.Contexts;
+using Manero.Controllers;
+using Manero.Models.Entities;
+using Manero.Services;
+using Manero.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manero.Tests;
+
+public class OrderHistoryServiceTests
+{
+    [Fact]
+    public async Task GetOrdersAsync_ReturnsEmptyList_WhenNoOrdersExist()
+    {
+        // Arrange
+        var options = new DbContextOptionsBuilder<DataContext>()
+            .UseInMemoryDatabase(databaseName: "TestDatabase")
+            .Options;
+
+        using var arrangeContext = new DataContext(options);
+
+        using var testContext = new DataContext(options);
+        var service = new OrderHistoryService(null!, testContext);
+
+        // Act
+        var result = await service.GetOrdersAsync("testUserId");
+
+        // Assert
+        Assert.Empty(result);
+    }
+
+    [Fact]
+    public async Task GetOrdersAsync_ReturnsOrdersForLoggedInUser()
+    {
+        // Arrange
+        var userId = "testUserId";
+
+        var user = new UserEntity
+        {
+            Id = userId,
+            FirstName = "John",
+            LastName = "Doe",
+        };
+
+        var orders = new List<CheckoutEntity>
+            {
+                new CheckoutEntity
+                {
+                    Order = new OrderEntity
+                    {
+                        UserId = userId,
+                    },
+                    StatusCode = new StatusCodeEntity
+                    {
+                        StatusName = "Delivered",
+                    }
+                },
+            };
+
+        var options = new DbContextOptionsBuilder<DataContext>()
+            .UseInMemoryDatabase(databaseName: "test_database")
+            .Options;
+
+        using (var context = new DataContext(options))
+        {
+            context.Users.Add(user);
+            context.CheckoutEntities.AddRange(orders);
+            context.SaveChanges();
+        }
+
+        var mockContext = new DataContext(options);
+
+        var service = new OrderHistoryService(null!, mockContext);
+
+        // Act
+        var result = await service.GetOrdersAsync(userId);
+
+        // Assert
+        Assert.NotNull(result);
+    }
+}
---
+++ Manero/Services/OrderHistoryService.cs
@@ -0,0 +1,45 @@
+﻿using Manero.Contexts;
+using Manero.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Manero.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace Manero.Services;
+
+public class OrderHistoryService
+{
+    private readonly UserManager<UserEntity> _userManager;
+    private readonly DataContext _context;
+
+    public OrderHistoryService(UserManager<UserEntity> userManager, DataContext dataContext)
+    {
+        _userManager = userManager;
+        _context = dataContext;
+    }
+
+    public async Task<List<OrderHistoryViewModel>> GetOrdersAsync(string userId)
+    {
+        var orders = await _context.CheckoutEntities
+            .Where(x => x.Order.UserId == userId)
+            .Include(x => x.StatusCode)
+            .Include(x => x.Order)
+            .Select(x => new OrderHistoryViewModel
+            {
+                Id = x.OrderId,
+                Created = x.Order.Created.HasValue ? x.Order.Created.Value : default,
+                Status = x.StatusCode.StatusName,
+                UpdateStatusDate = x.UpdateStatusDate,
+                TotalPrice = x.Order.TotalPrice,
+                Products = x.Order.OrderRows.Select(x => new OrderProductViewModel
+                {
+                    ProductArticleNumber = x.ProductArticleNumber,
+                    Quantity = x.Quantity,
+                    ProductPrice = x.ProductPrice,
+                }).ToList()
+            })
+            .ToListAsync();
+
+        return orders;
+    }
+}
