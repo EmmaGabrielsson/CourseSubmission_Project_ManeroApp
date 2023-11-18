e714648ff81b8948962d4d6d1ccd39c71019e8b6
tommyBjorkman <tommy.bjorkman@utb.ecutbildning.se>
2023-11-18 21:28:11 +0100

    fixed viewpath for carousel test

--- Manero.Tests/CarouselTest.cs
+++ Manero.Tests/CarouselTest.cs
@@ -1,27 +1,37 @@
-﻿using System;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 using Xunit;
 
-namespace Manero.Tests;
-
-
-
-public class CarouselTest
+namespace Manero.Tests
 {
-    private const string ViewPath = "C:\\Users\\tommy\\source\\repos\\CourseSubmission_Project_ManeroApp\\Manero\\Views\\Partials\\_CarouselPartial.cshtml"; // Replace with the actual path to your view
-
-    [Fact]
-    public void Test_Correct_Js_File_Is_Loaded()
+    public class CarouselTest
     {
-        string expectedJsFileName = "carousel.js";
+        private readonly string _viewPath;
 
-        string viewContent = File.ReadAllText(ViewPath);
+        public CarouselTest()
+        {
+            _viewPath = Path.Combine(GetProjectPath(), "Views", "Partials", "_CarouselPartial.cshtml");
+        }
 
-        Assert.True(ContainsJsFile(viewContent, expectedJsFileName));
-    }
-    private bool ContainsJsFile(string content, string jsFileName)
-    {
-        string pattern = $"<script\\s+.*src\\s*=\\s*['\"](.*/{jsFileName})['\"].*></script>";
-        return Regex.IsMatch(content, pattern, RegexOptions.IgnoreCase);
+        [Fact]
+        public void Test_Correct_Js_File_Is_Loaded()
+        {
+            string expectedJsFileName = "carousel.js";
+
+            string viewContent = File.ReadAllText(_viewPath);
+
+            Assert.True(ContainsJsFile(viewContent, expectedJsFileName));
+        }
+
+        private bool ContainsJsFile(string content, string jsFileName)
+        {
+            string pattern = $"<script\\s+.*src\\s*=\\s*['\"](.*/{jsFileName})['\"].*></script>";
+            return Regex.IsMatch(content, pattern, RegexOptions.IgnoreCase);
+        }
+
+        // Helper method to get the project path
+        private string GetProjectPath()
+        {
+            return Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..", "..", "..", "Manero");
+        }
     }
 }
