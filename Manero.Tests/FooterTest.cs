780e53760d56bd3d5632555032f588b526150adc
tommyBjorkman <tommy.bjorkman@utb.ecutbildning.se>
2023-11-19 15:34:06 +0100

    tried to fix footer test

--- Manero.Tests/FooterTest.cs
+++ Manero.Tests/FooterTest.cs
@@ -14,12 +14,16 @@
         {
             // Arrange
             string expectedText = "@media (max-width: 1400px) {\r\n    .footer-big {\r\n        display: none;\r\n    }\r\n}";
+            string expectedText2 = "@media (max-width: 1400px) {\r\n               .footer-big {\r\n                   display: none;\r\n               }\r\n           }";
 
             // Act
             string cssContent = File.ReadAllText(_cssFilePath);
 
             // Assert
-            Assert.Contains(expectedText, cssContent);
+            if (!(cssContent.Contains(expectedText) || cssContent.Contains(expectedText2)))
+            {
+                Assert.True(false, "The CSS content did not match any expected patterns.");
+            }
         }
         private string GetProjectPath()
         {
